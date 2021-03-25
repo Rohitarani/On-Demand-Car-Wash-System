fb1dc763bda93c73cb02b9f5f0a20b1e15b0ed37
rohrani <rohrani@LIN22004056.corp.capgemini.com>
2021-03-25 12:35:49 +0530

    commit 3

--- CarWashSystem/src/main/java/com/cg/CarWash/Dao/CarWashDao.java
+++ CarWashSystem/src/main/java/com/cg/CarWash/Dao/CarWashDao.java
@@ -4,7 +4,7 @@ import com.cg.CarWash.Entity.User;
 
 public interface CarWashDao {
 	public boolean addUser(User user);
-	
+	//public boolean deleteUser(User user);
 	//public boolean editUser(User user);
 	//public User viewUser(String userId);
 	
